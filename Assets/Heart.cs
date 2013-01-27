387d4af856c4b8d381af1da1aac7fbcca44d58ed
Garry <garry.wlsgmail.com>
2013-01-27 08:30:56 +0100

    Spawn globules at start

--- Assets/Heart.cs
+++ Assets/Heart.cs
@@ -13,6 +13,13 @@ public class Heart
     public int heartForLevel = 15;
     public int heartLevel;
 
+    public float globuleSpawnMinDelay = 3;
+    public float globuleSpawnMaxDelay = 7;
+    public float globuleSpawnNextTime;
+    public int globuleToSpawn;
+
+    public int maxGlobuleCountByLevel = 10;
+
     public Heart(WorldGrid _world, Cell _cell, Vector2 _position)
     {
         position = _position;
@@ -27,6 +34,9 @@ public class Heart
         ressourceConsumer.OnRessourceConsumed += new RessourceConsumer.RessourceEvent(ressourceConsumer_OnRessourceConsumed);
 
         cell.ressourceConsumer = ressourceConsumer;
+
+        globuleToSpawn = 5;
+        globuleSpawnNextTime = Time.time + Random.Range(globuleSpawnMinDelay, globuleSpawnMaxDelay);
     }
 
     bool ressourceConsumer_OnRessourceConsumed(RessourceType type)
@@ -42,6 +52,22 @@ public class Heart
             heartConsumed = 0;
             heartLevel++;
         }
+
+        if (globuleToSpawn > 0 && Time.time > globuleSpawnNextTime && worldGrid.globules.Count < maxGlobuleCountByLevel * (heartLevel + 1))
+        {
+            SpawnGlobule(position + new Vector2(0, 1));
+            globuleToSpawn--;
+
+            globuleSpawnNextTime = Time.time + Random.Range(globuleSpawnMinDelay, globuleSpawnMaxDelay);
+        }
     }
 
+    public void SpawnGlobule(Vector2 pos)
+    {
+        var globule = new Globule(worldGrid, pos);
+        globule.type = RessourceType.None;
+        globule.movement.speed = UnityEngine.Random.Range(worldGrid.particleSpeedMin, worldGrid.particleSpeedMax);
+
+        worldGrid.globules.Add(globule);
+    }
 }
